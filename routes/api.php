ac1384dddf2ba9d409b61a0e5c12f1e1d28d382a
caiiiok254 <kiskamiska2000@gmail.com>
2020-03-30 22:01:30 +0700

    Api and routes is done. All info can be sent and delivered via json. Need some help with frontend.

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::get('/ingredients', 'IngredientController@index');
-Route::post('/recipes', 'RecipeController@save');
-Route::get('/recipe/{id}', 'RecipeController@fetch');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/ingredients', 'IngredientController@index');
+Route::post('/recipes', 'RecipeController@save');
+Route::get('/recipe/{id}', 'RecipeController@fetch');
+Route::post('/recipes/preview', 'RecipeController@preview');
